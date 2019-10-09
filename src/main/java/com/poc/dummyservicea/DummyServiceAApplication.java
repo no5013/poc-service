5b07258b1d7d0f0ec6fccc19fc4e9d61ee29bce9
Phromma <wathanyu.phromma@accenture.com>
2019-10-09 15:43:52 +0700

    create endpoint for greeting

--- src/main/java/com/poc/dummyservicea/DummyServiceAApplication.java
+++ src/main/java/com/poc/dummyservicea/DummyServiceAApplication.java
@@ -1,13 +1,21 @@
 package com.poc.dummyservicea;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DummyServiceAApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DummyServiceAApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "What the f*ck";
+    }
 }
