282cff3899bb76c94b1ddb52861c5d869ed93439
Phromma <wathanyu.phromma@accenture.com>
2019-10-10 12:28:49 +0700

    Add endpoint to call another service

--- src/main/java/com/poc/dummyservicea/DummyServiceAApplication.java
+++ src/main/java/com/poc/dummyservicea/DummyServiceAApplication.java
@@ -1,14 +1,25 @@
 package com.poc.dummyservicea;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class DummyServiceAApplication {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(DummyServiceAApplication.class, args);
@@ -17,5 +28,20 @@ public class DummyServiceAApplication {
     @GetMapping("/hello")
     public String hello(){
         return "What the f*ck";
+    }
+
+    @GetMapping("/hello/{service}")
+    public String helloFromAnotherService(@PathVariable String service){
+        return restTemplate.exchange(
+                String.join("","http://",service),
+                HttpMethod.GET,
+                new HttpEntity<>(new HttpHeaders()),
+                String.class
+        ).getBody();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
